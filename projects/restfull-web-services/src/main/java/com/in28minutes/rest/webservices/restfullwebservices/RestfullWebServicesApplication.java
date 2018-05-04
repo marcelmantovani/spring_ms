880799cc35bf38b6585174da83cc9507fcf3c1c7
Marcel Mantovani <37183770+marcelmantovani@users.noreply.github.com>
2018-05-03 23:52:30 -0400

    simplifying the code for internationalization

--- projects/restfull-web-services/src/main/java/com/in28minutes/rest/webservices/restfullwebservices/RestfullWebServicesApplication.java
+++ projects/restfull-web-services/src/main/java/com/in28minutes/rest/webservices/restfullwebservices/RestfullWebServicesApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.support.ResourceBundleMessageSource;
 import org.springframework.web.servlet.LocaleResolver;
-import org.springframework.web.servlet.i18n.SessionLocaleResolver;
+import org.springframework.web.servlet.i18n.AcceptHeaderLocaleResolver;
 
 @SpringBootApplication
 public class RestfullWebServicesApplication {
@@ -19,19 +19,9 @@ public class RestfullWebServicesApplication {
 	
 	@Bean
 	public LocaleResolver localeResolver() {
-		SessionLocaleResolver localeResolver = new SessionLocaleResolver();
+		AcceptHeaderLocaleResolver localeResolver = new AcceptHeaderLocaleResolver();
 		localeResolver.setDefaultLocale(Locale.US);
 		return localeResolver;
-	}
-	
-	@Bean
-	public ResourceBundleMessageSource messageSource() {
-		
-		ResourceBundleMessageSource messageSource = new ResourceBundleMessageSource();
-		messageSource.setBasename("messages");
-		
-		return messageSource;
-		
 	}
 	
 }
