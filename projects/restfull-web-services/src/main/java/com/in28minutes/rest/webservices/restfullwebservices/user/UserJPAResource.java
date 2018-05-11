9c91d43973b1b7cc152558b789e42fd8013916ab
Marcel Mantovani <37183770+marcelmantovani@users.noreply.github.com>
2018-05-10 22:45:35 -0400

    converted GET methods to JPA

---
+++ projects/restfull-web-services/src/main/java/com/in28minutes/rest/webservices/restfullwebservices/user/UserJPAResource.java
@@ -0,0 +1,73 @@
+package com.in28minutes.rest.webservices.restfullwebservices.user;
+
+import static org.springframework.hateoas.mvc.ControllerLinkBuilder.linkTo;
+import static org.springframework.hateoas.mvc.ControllerLinkBuilder.methodOn;
+
+import java.net.URI;
+import java.util.List;
+import java.util.Optional;
+
+import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.hateoas.Resource;
+import org.springframework.hateoas.mvc.ControllerLinkBuilder;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+@RestController
+public class UserJPAResource {
+
+	@Autowired
+	private UserDaoService userService;
+	@Autowired
+	private UserRepository userRepository;
+	
+	@GetMapping(path = "/jpa/users")
+	public List<User> retrieveAllUsers(){
+		return userRepository.findAll();
+		
+	}
+	
+	@GetMapping("/jpa/users/{id}")
+	public Resource<User> retriveUser(@PathVariable int id) {
+		Optional<User> user = userRepository.findById(id);
+		if (!user.isPresent()) {
+			throw new UserNotFoundException("id-"+id);
+		}
+		
+		Resource<User> resource = new Resource<User>(user.get());
+		ControllerLinkBuilder linkTo = linkTo(methodOn(this.getClass()).retrieveAllUsers());
+		resource.add(linkTo.withRel("all-users"));
+		
+		return resource;
+	}
+	
+	@PostMapping(path = "/jpa/users")
+	public ResponseEntity<Object> createUser(@Valid @RequestBody User user) {
+		//Store the newly saved user in a new local object
+		User savedUser = userService.save(user);
+		//in order to return the URI of new user, it is broken in into 3 parts
+		//fromCurrentRequest() returns the /users part, as in the mapping
+		//.path("/{id}").buildAndExpand(savedUser.getId()) appends to /users the id of newly saved user
+		//.toUri() converts into URI
+		URI location = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(savedUser.getId()).toUri();
+		return ResponseEntity.created(location).build();
+	}
+	
+	@DeleteMapping("/jpa/users/{id}")
+	public void deleteUser(@PathVariable int id) {
+		User user = userService.deleteById(id);
+		if (user == null) {
+			throw new UserNotFoundException("id-"+id);
+		}
+	}
+
+	
+}
