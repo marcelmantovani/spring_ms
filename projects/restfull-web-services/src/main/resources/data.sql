7bd076319f1228d1b1d98982502e9be8f8a1c358
Marcel Mantovani <37183770+marcelmantovani@users.noreply.github.com>
2018-05-10 22:27:08 -0400

    made User class be managed by JPA, enabled H2 console

---
+++ projects/restfull-web-services/src/main/resources/data.sql
@@ -0,0 +1,5 @@
+insert into user values (1, sysdate(), 'Marcel');
+insert into user values (2, sysdate(), 'Camila');
+insert into user values (3, sysdate(), 'Arthur');
+insert into user values (4, sysdate(), 'Victor');
+insert into user values (5, sysdate(), 'Gabriela');
