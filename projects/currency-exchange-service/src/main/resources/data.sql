b5bd92592de0d33efaabe2c0adf0861585ee0330
Marcel Mantovani <37183770+marcelmantovani@users.noreply.github.com>
2018-05-18 19:50:52 -0400

    added h2 and jpa as dependencies, started creating the entity in h2

---
+++ projects/currency-exchange-service/src/main/resources/data.sql
@@ -0,0 +1,3 @@
+insert into Exchange_Value (id, currency_from, currency_to, conversion_multiple, port) values (1000, 'USD','EUR',0.8478, 0);
+insert into Exchange_Value (id, currency_from, currency_to, conversion_multiple, port) values (1001, 'USD','BRL',3.6951, 0);
+insert into Exchange_Value (id, currency_from, currency_to, conversion_multiple, port) values (1002, 'USD','CAD',1.2836, 0);
